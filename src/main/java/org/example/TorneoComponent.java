0c4be0ee8d7efa2890f042ba3f9b9ce9fe3963a5
marquinhos03 <mliguempibozzano@gmail.com>
2025-07-04 22:43:17 -0400

    Organizar Logica/GUI

--- src/main/java/org/example/TorneoComponent.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.util.List;
-
-public abstract class TorneoComponent {
-    public abstract String getNombre();
-    public abstract List<TorneoComponent> getComponentes();
-    public abstract void agregar(TorneoComponent component);
-}
--- src/main/java/org/example/TorneoObserver.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public interface TorneoObserver {
-    void actualizar(String mensaje);
-}
