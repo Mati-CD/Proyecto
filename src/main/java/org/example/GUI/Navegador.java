88204fc1efd6c14fa2d1172dcb22d3615e131c58
marquinhos03 <mliguempibozzano@gmail.com>
2025-07-10 23:06:00 -0400

    Navegador: - Se introduce el método mostrarPanel() que reduce la duplicación de código en los metodos mostrarPanel(Usuario, Organizador, etc).

---
+++ src/main/java/org/example/GUI/Navegador.java
@@ -0,0 +1,61 @@
+package org.example.GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.Stack;
+
+public class Navegador {
+    private PanelPrincipal panelPrincipal;
+    private Stack<ActionListener> historial;
+
+    // actionAssigner es un objeto que implementa ActionAssigner
+    private ActionAssigner actionAssigner;
+
+    public Navegador(PanelPrincipal panelPrincipal) {
+        this.panelPrincipal = panelPrincipal;
+        this.historial = new Stack<>();
+        this.actionAssigner = new AbstractActionAssigner(this) {};
+    }
+
+    private <T extends JPanel & PanelConfigurable> void mostrarPanel(
+            PanelType panelType,
+            ActionListener actionToPushHistorial) {
+
+        if(actionToPushHistorial != null) {
+            historial.push(actionToPushHistorial);
+        }
+
+        T panel = panelPrincipal.getPanel(panelType);
+        panel.inicializar(actionAssigner);
+        panelPrincipal.cambiarPanel(panel);
+    }
+
+    public void mostrarPanelInicio() {
+        // El historial se limpia por completo si se vuelve al inicio
+        historial.clear();
+        PanelInicio panelInicio = panelPrincipal.getPanel(PanelType.INICIO);
+
+        // Inicializar
+        panelInicio.inicializar(actionAssigner);
+        panelPrincipal.cambiarPanel(panelInicio);
+    }
+
+    public void mostrarPanelUsuario() {
+        mostrarPanel(PanelType.USUARIO, e -> mostrarPanelInicio());
+    }
+    public void mostrarPanelEstadoActualTorneo() {
+        mostrarPanel(PanelType.ESTADO_ACTUAL, e -> mostrarPanelOrganizador());
+    }
+    public void mostrarPanelOrganizador() {
+        mostrarPanel(PanelType.ORGANIZADOR, e -> mostrarPanelInicio());
+    }
+    public void mostrarPanelCrearTorneo() {
+        mostrarPanel(PanelType.CREAR_TORNEO, e -> mostrarPanelOrganizador());
+    }
+    public void mostrarPanelInscribirParticipantes() {
+        mostrarPanel(PanelType.INSCRIBIR_PARTICIPANTES, e -> mostrarPanelOrganizador());
+    }
+    public void mostrarPanelIniciarTorneo() {
+        mostrarPanel(PanelType.INICIAR_TORNEO, e -> mostrarPanelOrganizador());
+    }
+}
